53d6506c9693462ad36d8fea5d76bb5eb9e2a17b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-03-09 20:25:25 +0300

    change files

--- homeworks/src/p2-homeworks/h2/HW2.tsx
+++ homeworks/src/p2-homeworks/h2/HW2.tsx
@@ -7,7 +7,7 @@ export type AffairPriorityType = ' high' | ' middle' | ' low'
 export type AffairType = {
     _id: number
     name: string
-    priority: string
+    priority: AffairPriorityType
 }
 export type FilterType = ' all' | AffairPriorityType
 
