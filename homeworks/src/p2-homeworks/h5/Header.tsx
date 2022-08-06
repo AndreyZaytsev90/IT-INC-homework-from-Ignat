18d410fa46b7bdb69f890665cf180111d114bafe
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-08-06 13:49:16 +0300

    Added menu with links

--- homeworks/src/p2-homeworks/h5/Header.tsx
+++ homeworks/src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
+import style from "./css/header.module.css"
+import Arrow from "./svg/arrow.svg"
 
 function Header() {
+
+    const [active, setActive] = useState(false)
+    const onClickHandler = () => setActive(!active)
+
     return (
-        <div>
-            <NavLink to={'/pre-junior'}>preJunior</NavLink>---
-            <NavLink to={'/junior'}>Junior</NavLink>---
-            <NavLink to={'/junior+'}>Junior+</NavLink>
+        <div className={active? `${style.header_active}`:`${style.header}` }>
+        {/*    <button style={Arrow} className={style.arrow} onClick={onClickHandler}>Arrow</button>*/}
+            <img src={Arrow} className={active? `${style.arrow_active}`:`${style.arrow}` } onClick={onClickHandler} alt={Arrow}/>
+            <div className={style.links}>
+                <NavLink to={'/pre-junior'}>pre-Junior</NavLink>
+                <NavLink to={'/junior'}>Junior</NavLink>
+                <NavLink to={'/junior+'}>Junior+</NavLink>
+            </div>
         </div>
     )
 }
--- homeworks/src/p2-homeworks/h5/pages/Error404.tsx
+++ homeworks/src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import notFound from './../svg/404notFound.jpg'
+import style from './../css/error.module.css'
 
 function Error404() {
     return (
         <div>
-            <div>404</div>
+            <img src={notFound} className={style.error} alt={"404notFound"}/>
+           {/* <div>404</div>
             <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
         </div>
     )
 }
