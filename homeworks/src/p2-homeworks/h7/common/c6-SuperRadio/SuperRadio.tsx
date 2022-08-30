b8fe760dc91c0095a29143a6520d84343b5a4bf8
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-08-30 12:54:48 +0300

    Fixed bugs

--- homeworks/src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ homeworks/src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -23,7 +23,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     onChange && onChange(e)
   }
 
-  const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+  const mappedOptions: any[] = options ? options.map((o, i) => (
     <label key={name + '-' + i}>
       <input
         type={'radio'}
@@ -35,7 +35,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
       />
       {o}
     </label>
-  )) : []
+  )) : [] // map options with key
 
   return (
     <>
