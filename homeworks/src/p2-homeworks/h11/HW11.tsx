d977e1b4c8f9b90db767824f9ec21edb5d9b5512
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-19 21:44:01 +0300

    change file

--- homeworks/src/p2-homeworks/h11/HW11.tsx
+++ homeworks/src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,45 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(10)
+  const [value2, setValue2] = useState(100)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  const changeValue = () => {
+    setValue1(value1)
+  }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+    return(
+    <div>
+      <hr/>
+      <h2 style={{marginLeft: 20}}>homeworks 11</h2>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+      {/*should work (должно работать)*/}
+      <div style={{marginLeft: 20}}>
+        <span>{value1}</span>
+        <SuperRange
+          value={value1}
+          changeValue={changeValue}
+          // сделать так чтоб value1 изменялось
+        />
+      </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+      <div style={{marginLeft: 20, marginTop: 5}}>
+        <span>{value1}</span>
+        <SuperDoubleRange
+          // сделать так чтоб value1 и value2 изменялось
+        />
+        <span>{value2}</span>
+      </div>
+
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW11
